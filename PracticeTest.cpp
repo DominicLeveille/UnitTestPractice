7c3b63ea24bc6cb8ee9230edd32f4cb18878f7f4
DominicLeveille <dominic24@cox.net>
2020-11-16 16:07:31 -0800

    add tests for functions

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,53 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("RaceCar");
     ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_hard_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("MayaMoodyBabyDoomAYam");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_descending)
+{
+    Practice obj;
+	int a, b ,c;
+	a = 1;
+	b = 2;
+	c = 3;
+    bool actual = obj.sortDescending(a, b, c);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_medium_descending)
+{
+    Practice obj;
+    int a, b ,c;
+	a = 1;
+	b = 2;
+	c = 3;
+    bool actual = obj.sortDescending(c, b, a);
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_not_descending)
+{
+    Practice obj;
+    int a, b ,c;
+	a = 2;
+	b = 2;
+	c = 2;
+    bool actual = obj.sortDescending(c, b, a);
+    ASSERT_FALSE(actual);
 }
